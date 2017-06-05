0723b1ab006a37bfdff8c8e11ccc1f3ad1c1c4dc
VANESSA\Caleb <vash4512@gmail.com>
2017-06-04 20:55:20 -0700

    no message

---
+++ NFLPlayerbenchmark/src/player.java
@@ -0,0 +1,19 @@
+// class name player
+public class player {
+//Milestone 2 NFL player Class using http://www.nfl.com/stats/player, and https://www.hometeamsonline.com/teams/popups/Glossary.asp?s=football
+	// Method used
+	public static void main(String[] args){
+		
+		//int numbers or letters with no decimals negative or positive
+		int playerName = name, playerNumber = number, playerHeight = height, playerWeight = weight, playerAge = age,
+		 playerGames = games, playerGamesStarted = gamesStarted, playerCompletions = completions, playerAttempts = attempts,
+		 playerPassingYards = passingYards, playerPassingTouchDowns = passingtouchDowns, playerInterceptions = interceptions,
+		 playerSacks = sacks, playerSackYards = sackYards, playerRushingAttempts = rushingAttempts, playerRushingYards = rushingYards,
+		 playerRushingTouchdowns = rushingTouchdowns, playerFumbles = fumbles, playerFumblesLost = fumblesLost;
+		
+		//double data with decimals that have numbers on other side of decimal
+		double playerPercentage = percentage, playerPassingAverage = passingAverage, playerPassingRate = passingRate, 
+		playerRushingAverage = rushingAverage, playerRushingRate = rushingRate;
+		
+	}
+}
