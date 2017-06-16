7d35c6b157d5cee41abfc866364e68ddab4aea2b
VANESSA\Caleb <vash4512@gmail.com>
2017-06-16 15:08:02 -0700

    no message

---
+++ Final Project/Inheritance/NFLPlayer.java
@@ -0,0 +1,115 @@
+import javax.swing.JFrame;
+
+//Class NFL PLAYER abstract creates class, but cannot create objects from it
+abstract public class NFLPlayer {
+	
+		
+			
+			//Player name and stats
+			String PlayerName = "";
+			
+			int PlayerNumber = 0, 
+				PlayerAge = 0;
+
+			
+			//double data with decimals that have numbers on other side of decimal
+			double  PlayerPassingAverage = 0.0, 
+					PlayerHeight = 0.0,
+					PlayerWeight = 0.0;
+			
+			//constructor getter and setters
+			public NFLPlayer(){
+			}
+			
+
+		    public void setPlayerName(String newPlayerName) {
+	            PlayerName = newPlayerName;
+	        }
+	        
+	        public void setPlayerNumber(int newPlayerNumber) {
+	            PlayerNumber= newPlayerNumber;
+	        }
+	        
+	        public void setPlayerAge(int newPlayerAge) {
+	            PlayerAge = newPlayerAge;
+	        }
+	        
+	        public void setPlayerHeight(double newPlayerHeight) {
+	            PlayerHeight = newPlayerHeight;
+	        }
+	        
+	        public void setPlayerWeight(double newPlayerWeight) {
+	            PlayerWeight = newPlayerWeight;
+	        }
+	        
+	        public void setPlayerPassingAverage(double newPlayerPassingAverage) {
+	            PlayerPassingAverage = newPlayerPassingAverage;
+	        }
+			
+			public String getPlayerName() {
+				return PlayerName;
+			}
+		
+			public int getPlayerNumber() {
+				return PlayerNumber;
+			}
+			
+		
+			public int getPlayerAge() {
+				return PlayerAge;
+			}
+			
+		
+			public double getPlayerHeight() {
+				return PlayerHeight;
+			}
+			
+		
+			public double getPlayerWeight() {
+				return PlayerWeight;
+			}
+			
+				
+			public double GetPlayerPassingAverage() {
+				return PlayerPassingAverage;
+			}
+		
+			
+		// Method used
+		public static void main(String[] args){	
+			
+			//gui
+		
+		/*	OffensivePlayer bucky = new OffensivePlayer();
+			bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			bucky.setSize(275,180);
+			bucky.setVisible(true);
+*/
+		}
+	}
+
+
+	/* 
+	 Create the NFLPlayer class.
+
+
+	Assignment Description: 
+
+	Students will be required to fully implement a class,
+	 making use of at least two variable types to define its 
+	 properties.
+	 
+	Create the NFLPlayer class and add its properties.
+	Include player statistics found on the NFL website.
+	Use the variable types learned so far to create the 
+	player properties.
+	Deliverables:
+
+	NFLPlayer class
+
+	Submit the NFLPlayer class to your final project
+	Git Repository folder.
+	http://www.nfl.com/stats/player
+	 */
+
+
---
+++ Final Project/Inheritance/OffensivePlayer.java
@@ -0,0 +1,19 @@
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class OffensivePlayer extends NFLPlayer{
+	
+	private JLabel item1;
+	
+	public OffensivePlayer() {
+	//	super("The Title Bar");
+	//	setLayout(new FlowLayout()/*default layout*/);
+	/*	
+		item1 = new JLabel("this is a sentencew");
+		item1.setToolTipText("this is gonna show up on hover");
+		add(item1);
+		*/
+	}
+
+}
